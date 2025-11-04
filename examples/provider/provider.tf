bc12b5ee8838cf2db31751e5a01c5aa649f51855
Jason Kossis <jkossis@gmail.com>
2025-11-04 13:53:52 -0500

    fixing generate

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,16 +1,4 @@
-terraform {
-  required_providers {
-    garage = {
-      source = "jkossis/garage"
-    }
-  }
-}
-
 provider "garage" {
   endpoint = "http://localhost:3903"
-  token    = "your-admin-token-here"
+  token    = "your-garage-admin-token"
 }
-
-# Alternatively, use environment variables:
-# export GARAGE_ENDPOINT="http://localhost:3903"
-# export GARAGE_TOKEN="your-admin-token-here"
---
+++ examples/resources/garage_bucket/import.sh
@@ -0,0 +1,2 @@
+# Garage buckets can be imported using the bucket ID
+terraform import garage_bucket.example bucket-id-here
